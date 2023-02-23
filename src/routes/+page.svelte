4aaf52fec646a712d521f39db7aad66389e62be0
Quinten Coret <Qcoret@gmail.com>
2023-02-23 22:26:12 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div>
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="/about">/about</a> to navigate.</p>
+</div>
+
+{#each Array(25) as _}
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In fringilla dui in leo finibus luctus.
+    Nunc porttitor posuere nisi, suscipit maximus dolor dignissim a. Praesent tristique odio justo,
+    feugiat molestie tortor eleifend id. In euismod ligula ipsum, in auctor leo vulputate sit amet.
+    Aenean dui nulla, suscipit quis condimentum quis, tristique vitae erat. Donec sed eleifend nibh,
+    id eleifend libero. Integer et vulputate libero.
+  </p>
+{/each}
+
+<style>
+  div {
+    position: fixed;
+    background: white;
+  }
+
+  div,
+  p {
+    width: 25rem;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<div>
+  <h1>Welcome to About</h1>
+  <p>Visit <a href="/">/index</a> to navigate.</p>
+</div>
+
+{#each Array(25) as _}
+  <p>
+    Donec suscipit orci leo, et auctor turpis tincidunt non. Proin fermentum aliquam magna sed
+    placerat. Ut congue neque vel ante bibendum, vitae eleifend ex lacinia. Integer consectetur
+    pellentesque quam, eu lacinia odio. Donec dictum pharetra nunc. Cras suscipit in felis a
+    aliquam. Curabitur nec vehicula ipsum, ut iaculis nisl. Pellentesque habitant morbi tristique
+    senectus et netus et malesuada fames ac turpis egestas. Nunc finibus est quis massa pharetra, ut
+    venenatis justo lacinia. Duis rutrum velit quis arcu lobortis, ac tincidunt odio dapibus.
+    Vestibulum varius quam non lorem lobortis viverra. Ut rhoncus sapien at dictum convallis.
+  </p>
+{/each}
+
+<style>
+  div {
+    position: fixed;
+    background: white;
+  }
+
+  div,
+  p {
+    width: 25rem;
+  }
+</style>
